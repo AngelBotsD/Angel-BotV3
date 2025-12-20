fc13a6d05ba7b558d50f646c2390e2a8d64c7752
AngelBotsD <angelperez22448@gmail.com>
2025-12-19 21:16:00 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -185,7 +185,7 @@ user.name = nuevo
 }} catch {}
 const chat = global.db.data.chats[m.chat]
 const settings = global.db.data.settings[this.user.jid]  
-const isROwner = [...global.owner.map((number) => number)].map(v => v.replace(/[^0-9]/g, "") + "@lid").includes(m.sender)
+const isPrems = isROwner || user?.premium === true
 const isOwner = isROwner || m.fromMe
 const isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, "") + "@lid").includes(m.sender) || user.premium == true
 const isOwners = [this.user.jid, ...global.owner.map((number) => number + "@lid")].includes(m.sender)
