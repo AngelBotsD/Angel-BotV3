2243ccde6193328a7a33004fb9617dc9c33a97bc
AngelBotsD <angelperez22448@gmail.com>
2025-12-20 19:54:40 -0600

    Actualizar play2.js

--- plugins/descargas/play2.js
+++ plugins/descargas/play2.js
@@ -98,7 +98,7 @@ const handler = async (msg, { conn, text, usedPrefix, command }) => {
 }
 
 
-handler.command = ["play", "ytplay"]
+handler.command = ["play2", "ytplay2"]
 handler.help    = ["play <texto o url>"]
 handler.tags    = ["descargas"]
 
