9e8fa5f2ce7626c81e6028ad51aa387a19b17b91
AngelBotsD <angelperez22448@gmail.com>
2025-12-22 01:02:06 -0600

    Actualizar templateResponse.js

--- plugins/informacion/templateResponse.js
+++ plugins/informacion/templateResponse.js
@@ -1,1 +1,29 @@
-//xd
+let handler = async (m, { conn, usedPrefix, command }) => {
+
+    //m.react('🕑');
+
+    let txt = 'Pack 🔥';
+    let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
+
+    let buttons = [
+        {
+            buttonId: `.pack`,
+            buttonText: { displayText: "Ver más" },
+            type: 1
+        }
+    ];
+    await conn.sendMessage(
+        m.chat,
+        {
+            image: { url: img },
+            caption: txt,
+            buttons: buttons,
+            viewOnce: true
+        },
+        { quoted: m }
+    );
+};
+
+handler.command = ['pack'];
+
+export default handler;
