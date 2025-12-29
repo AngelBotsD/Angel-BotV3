a6fbf69ad12804495afe2c13d5929bc168cf0908
AngelBotsD <angelperez22448@gmail.com>
2025-12-29 17:12:12 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const phoneUtil = PhoneNumberUtil.getInstance()
 const { DisconnectReason, useMultiFileAuthState, MessageRetryMap, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser, Browsers } = await import('@whiskeysockets/baileys')
 import readline, { createInterface } from 'readline'
 import NodeCache from 'node-cache'
-import { initFantasma } from './plugins/grupos/fantasmas.js'
 const { CONNECTING } = ws
 const { chain } = lodash
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
@@ -179,7 +178,6 @@ maxIdleTimeMs: 60000,
 }
 
 global.conn = makeWASocket(connectionOptions)
-initFantasma(global.conn)
 if (!fs.existsSync(`./${sessions}/creds.json`)) {
 if (opcion === '2' || methodCode) {
 opcion = '2'
