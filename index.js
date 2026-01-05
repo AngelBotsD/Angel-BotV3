15eaafae80902e0710436277fa89d6a2f9cb5908
AngelBotsD <angelperez22448@gmail.com>
2026-01-04 20:44:47 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ const phoneUtil = PhoneNumberUtil.getInstance()
 const { DisconnectReason, useMultiFileAuthState, MessageRetryMap, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser, Browsers } = await import('@whiskeysockets/baileys')
 import readline, { createInterface } from 'readline'
 import NodeCache from 'node-cache'
-const { CONNECTING } = ws
 const { chain } = lodash
-const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 import {
   downloadContentFromMessage,
