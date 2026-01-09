f077991759c2e6ba298523a90b44c375a7417730
AngelBotsD <angelperez22448@gmail.com>
2026-01-09 17:56:25 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  '159606034665538',
-  '205819731832938',
-  '5714222810',
-  '447894206349'
+'159606034665538',
+'205819731832938',
+'5714222810',
+'447894206349'
 ]
 
 global.bot = {
-  name: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-  alias: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-  packname: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-  author: '𝖠𝗇𝗀𝖾𝗅',
-  session: '𝖠𝗇𝗀𝖾𝗅𝖡𝗈𝗍',
-  banner: 'https://files.catbox.moe/4k94dp.jpg'
+name: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+alias: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+packname: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+author: '𝖠𝗇𝗀𝖾𝗅',
+session: '𝖠𝗇𝗀𝖾𝗅𝖡𝗈𝗍',
+banner: 'https://files.catbox.moe/4k94dp.jpg'
 }
 
 global.namebot = global.bot.name
@@ -24,9 +26,19 @@ global.sessions = global.bot.session
 global.banner = global.bot.banner
 
 global.APIs = {
-  may: 'https://api.soymaycol.icu'
+sky: 'https://api-sky.ultraplus.click',
+may: 'https://api.soymaycol.icu'
 }
 
 global.APIKeys = {
-  may: process.env.MAY_API_KEY || 'may-e89378ce'
+sky: process.env.SKY_API_KEY || 'Angxlllll',
+may: process.env.MAY_API_KEY || 'may-e89378ce'
 }
+
+const file = fileURLToPath(import.meta.url)
+
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("config.js actualizado"))
+import(`${file}?update=${Date.now()}`)
+})
