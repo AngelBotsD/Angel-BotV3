08885a8e2513b6970930d0a28706a4c283d6aa52
AngelBotsD <angelperez22448@gmail.com>
2026-01-11 00:46:42 -0600

    Actualizar j.js

--- plugins/descargas/j.js
+++ plugins/descargas/j.js
@@ -1,15 +1,1 @@
-export async function all(m) {
-  if (!m.text) return
-
-  const t = m.text.trim().toLowerCase()
-
-  if (t === 'menu') {
-    m.text = '.menu'
-  }
-
-  if (t === 'owner') {
-    m.text = '.owner'
-  }
-}
-
-export default {}
+//xd
