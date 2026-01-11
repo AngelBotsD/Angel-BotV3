61a185fffa0a655726fcdcf51299728842b0aabd
AngelBotsD <angelperez22448@gmail.com>
2026-01-11 01:19:27 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,5 @@
 import { smsg } from "./lib/simple.js"
 import { fileURLToPath } from "url"
-import pkg from 'baileys_helper'
-const { sendButtons } = pkg
 import path, { join } from "path"
 import fs, { unwatchFile, watchFile } from "fs"
 import chalk from "chalk"
